cc93578456e0b2c5b44759746be840c91e4a19da
Chris Hua <hua.christopher@gmail.com>
2024-08-10 18:20:52 -0400

    fix: include files

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
+import {viteStaticCopy} from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
@@ -9,7 +10,22 @@ export default defineConfig({
         dts({
             insertTypesEntry: true,
         }),
-    ],
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js',
+                    dest: './'
+                },
+                {
+                    src: 'node_modules/@ricky0123/vad-web/dist/silero_vad.onnx',
+                    dest: './'
+                },
+                {
+                    src: 'node_modules/onnxruntime-web/dist/*.wasm',
+                    dest: './'
+                }
+            ]
+        })],
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
