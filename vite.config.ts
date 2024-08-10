b1c1cafbede703c9c4f5e1aed656fbf000a89cc3
Chris Hua <hua.christopher@gmail.com>
2024-08-10 17:40:34 -0400

    chore: extenralize buffer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
+import  inject  from '@rollup/plugin-inject';
 
 export default defineConfig({
   plugins: [
@@ -18,6 +19,7 @@ export default defineConfig({
       fileName: (format) => `taolun.${format}.js`,
     },
     rollupOptions: {
+        plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })],
       external: ['react', 'react-dom'],
       output: {
         globals: {
